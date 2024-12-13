bcd83c2d00f0d0ab7425b440f247c190be82f980
Vouchiko <vovakarpov2017@gmail.com>
2024-12-13 17:37:59 +0400

    Added maping

---
+++ DispatcherService.API/Mapping.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using DispatcherService.API.DTO;
+using DispatcherService.Domain.Entities;
+
+namespace DispatcherService.API;
+
+/// <summary>
+/// Класс для маппинга
+/// </summary>
+public class Mapping : Profile
+{
+    /// <summary>
+    /// Маппинг сущностей
+    /// </summary>
+    public Mapping()
+    {
+        // Маппинг для Transport
+        CreateMap<Transport, TransportDto>();
+        CreateMap<TransportDto, Transport>();
+        CreateMap<Transport, TransportFullDto>();
+        CreateMap<TransportFullDto, Transport>();
+        
+        //Driver
+        CreateMap<Driver, DriverDto>();
+        CreateMap<DriverDto, Driver>();
+        CreateMap<Driver, DriverFullDto>();
+        CreateMap<DriverFullDto, Driver>();
+
+        //Schedule
+        CreateMap<Schedule, SchedulesDto>();
+        CreateMap<SchedulesDto, Schedule>();
+        CreateMap<Schedule, SchedulesFullDto>();
+        CreateMap<SchedulesFullDto, Schedule>();
+
+    }
+}
